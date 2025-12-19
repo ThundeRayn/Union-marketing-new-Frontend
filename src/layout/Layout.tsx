f789c7562c30818dc52508ad750b2954300a6e5f
Your Name <tangshirong477@gmail.com>
2025-12-19 09:28:33 -0500

    favcon

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../blocks/Navbar";
+import Footer from "../blocks/Footer";
 
 const Layout = () => {
   return (
@@ -10,6 +11,7 @@ const Layout = () => {
         <Outlet /> 
         {/* Page content goes here */}
       </main>
+      <Footer/>
 
     </div>
   )

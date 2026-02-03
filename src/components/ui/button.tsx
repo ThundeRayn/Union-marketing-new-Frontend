dba2ad6f9281a68369ec6e095b839532230c68fc
Your Name <tangshirong477@gmail.com>
2026-02-03 14:06:18 -0500

    adjustion

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -11,7 +11,7 @@ const buttonVariants = cva(
       variant: {
         default: "bg-primary text-primary-foreground hover:bg-primary/90 hover:cursor-pointer",
         union: "bg-black text-white rounded-[50px] hover:bg-yellow-400 hover:text-black transition-all duration-500 ease-in-out",
-        price: "border border-black border-3 text-black rounded-none hover:border-gray-400 hover:text-gray-400 hover:cursor-pointer transition-all duration-300 ease-in-out",
+        price: "border border-black border-2 md:border-2 text-black rounded-none hover:border-gray-400 hover:text-gray-400 hover:cursor-pointer transition-all duration-300 ease-in-out",
         destructive:
           "bg-destructive text-white hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
         outline:

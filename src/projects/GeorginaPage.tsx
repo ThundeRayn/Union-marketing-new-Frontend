8b341fd159e885be50e8da831c9ee276d315f1ea
Your Name <tangshirong477@gmail.com>
2026-04-23 23:23:57 +0800

    updating

--- src/projects/GeorginaPage.tsx
+++ src/projects/GeorginaPage.tsx
@@ -1,5 +1,7 @@
 import Upbadge from "@/blocks/Upbadge"
 import ProjectBuilder from "@/blocks/ProjectBuilder"
+import ProjectButtonList from "@/blocks/ProjectButtonList"
+import type { ProjectButton } from "@/blocks/ProjectButtonList"
 import ProjectNavigation from "@/blocks/ProjectNavigation"
 import BackToHome from "@/components/BackToHome"
 import ProjectInfo from "@/components/ProjectInfo"
@@ -10,6 +12,14 @@ import projectsData from '@/data/projects.json'
 const project = projectsData.find(p => p.id === 'georgina')!
 
 const GeorginaPage = () => {
+  const buttons: ProjectButton[] = [
+    { id: 1, label: 'PRICE LIST', link: 'https://res.cloudinary.com/dqj2gwlpf/image/upload/v1776957488/Price_List-Georgina_Height_rwhlnu.pdf', target: '_blank' },
+    { id: 2, label: 'BROCHURE 1', link: 'https://res.cloudinary.com/dqj2gwlpf/image/upload/v1776957563/Georgina-Heights-By-Treasure-Hill-Brochure-Floor-Plans_gkbuss.pdf', target: '_blank' },
+    { id: 3, label: 'BROCHURE 2', link: 'https://res.cloudinary.com/dqj2gwlpf/image/upload/v1776957637/Georgina-Heights-Brochure_grjhkw.pdf', target: '_blank' },
+    { id: 4, label: 'SITE PLAN', link: 'https://res.cloudinary.com/dqj2gwlpf/image/upload/v1776957730/Site-Plan_ksa00d.pdf', target: '_blank' },
+    //{ id: 5, label: 'SITE PLAN', link: '#', target: '_blank' },
+  ]
+
   const images = [
     'https://res.cloudinary.com/dqj2gwlpf/image/upload/v1768278791/Georgina-Heights-Site-Plan-scaled_y38o3y.jpg'
   ];
@@ -31,6 +41,13 @@ const GeorginaPage = () => {
       
 
       <Map title="Site Plan" images={images} />
+
+      {/* Main Content Area */}
+      <div className="mx-auto flex flex-col items-center px-8 md:px-16 lg:px-24">
+        <div className="w-full max-w-2xl py-10 md:py-20">
+          <ProjectButtonList buttons={buttons} />
+        </div>
+      </div>
 
       <ProjectBuilder
         images={[

9feba1214b7b0b0487068a57fc624a3befc96735
Your Name <tangshirong477@gmail.com>
2026-05-01 22:19:38 +0800

    feat: add auth/callback and broker-portal routes to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import { useEffect, lazy } from 'react'
+import { useEffect, lazy, Suspense } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 import './App.css'
 import Layout from './layout/Layout'
+import { ProtectedRoute } from './components/ProtectedRoute'
 
 const HomePage = lazy(() => import('./pages/HomePage'))
 const ServicePage = lazy(() => import('./pages/ServicePage'))
@@ -10,6 +11,8 @@ const AboutPage = lazy(() => import('./pages/AboutPage'))
 const EventPage = lazy(() => import('./pages/EventPage'))
 const ProjectPage = lazy(() => import('./pages/ProjectPage'))
 const BrokerPortalLogin = lazy(() => import('./pages/BrokerPortalLogin'))
+const AuthCallback = lazy(() => import('./pages/AuthCallback'))
+const BrokerPortal = lazy(() => import('./pages/BrokerPortal'))
 const FifthPage = lazy(() => import('./projects/FifthPage'))
 const ElevenPage = lazy(() => import('./projects/ElevenPage'))
 const EversleyPage = lazy(() => import('./projects/EversleyPage'))
@@ -29,32 +32,41 @@ function App() {
   }, [])
 
   return (
-    <>
-      <BrowserRouter>
+    <BrowserRouter>
+      <Suspense fallback={null}>
         <Routes>
-          <Route path='/' element={<Layout />}>
+          {/* Routes without navbar/footer */}
+          <Route path="auth/callback" element={<AuthCallback />} />
+          <Route
+            path="broker-portal"
+            element={
+              <ProtectedRoute>
+                <BrokerPortal />
+              </ProtectedRoute>
+            }
+          />
+
+          {/* Routes with navbar/footer */}
+          <Route path="/" element={<Layout />}>
             <Route index element={<HomePage />} />
-            <Route path='service' element={<ServicePage />} />
-            <Route path='about' element={<AboutPage />} />
-            <Route path='event' element={<EventPage />} />
-            <Route path='project' element={<ProjectPage />} />
-            <Route path='login' element={<BrokerPortalLogin />} />
-              <Route path='projects/fifth' element={<FifthPage />} ></Route>
-              <Route path='projects/eleven' element={<ElevenPage />} ></Route>
-              <Route path='projects/eversley' element={<EversleyPage />} ></Route>
-              <Route path='projects/georgina' element={<GeorginaPage />} ></Route>
-              <Route path='projects/cgtower' element={<CGTowerPage />} ></Route>
-              <Route path='projects/abeja' element={<AbejaPage />} ></Route>
-              <Route path='projects/lakewilcox' element={<LakeWilcoxPage />} ></Route>
-              <Route path='projects/woodbridge' element={<WoodbridgePage />} ></Route>
-            <Route path='*' element={<div>404 Not Found</div>} />
-            
-            
+            <Route path="service" element={<ServicePage />} />
+            <Route path="about" element={<AboutPage />} />
+            <Route path="event" element={<EventPage />} />
+            <Route path="project" element={<ProjectPage />} />
+            <Route path="login" element={<BrokerPortalLogin />} />
+            <Route path="projects/fifth" element={<FifthPage />} />
+            <Route path="projects/eleven" element={<ElevenPage />} />
+            <Route path="projects/eversley" element={<EversleyPage />} />
+            <Route path="projects/georgina" element={<GeorginaPage />} />
+            <Route path="projects/cgtower" element={<CGTowerPage />} />
+            <Route path="projects/abeja" element={<AbejaPage />} />
+            <Route path="projects/lakewilcox" element={<LakeWilcoxPage />} />
+            <Route path="projects/woodbridge" element={<WoodbridgePage />} />
+            <Route path="*" element={<div>404 Not Found</div>} />
           </Route>
-          
         </Routes>
-      </BrowserRouter>
-    </>
+      </Suspense>
+    </BrowserRouter>
   )
 }
 
